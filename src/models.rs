4b7be04e359d42077558e42a51f1a74e3783ca18
Baakel <thrauglor@gmail.com>
2021-04-27 02:01:43 +0200

    Working route for adding recipes, started adding the users route

--- src/models.rs
+++ src/models.rs
@@ -11,23 +11,28 @@ pub struct Recipe {
     pub steps: Option<Vec<String>>,
     pub tipo: Option<String>,
     pub calories: Option<u16>,
-    pub carbohydrates: Option<u16>,
-    pub fat: Option<u16>,
-    pub protein: Option<u16>,
+    pub carbohydrates: Option<f32>,
+    pub fat: Option<f32>,
+    pub protein: Option<f32>,
+    pub servings: Option<String>,
     pub ingredients: Option<Vec<Ingredient>>
 }
 
 #[derive(Debug, Deserialize, Serialize, Validate)]
 pub struct User {
+    pub id: Option<Uuid>,
+    #[validate(length(min = 3))]
     pub username: String,
     #[validate(length(min = 10))]
     pub password: String,
     #[validate(email)]
     pub email: String,
+    pub role: Option<String>
 }
 
 #[derive(Debug, Deserialize, Serialize, Validate)]
 pub struct Ingredient {
     pub name: String,
     pub tipo: Option<String>,
+    pub amount: String,
 }
